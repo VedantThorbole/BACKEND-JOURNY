918d1400da308a850a854511dc90cbd9b680b805
Vedant Thorbole <vedantthorbole8@gmail.com>
2026-01-28 23:14:51 +0530

    Delete DAY-23 directory

--- DAY-23/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-const notesRouter = require("./routes/notes");
-
-app.use("/notes", notesRouter);
-
-app.listen(8000, () => console.log("Server running"));
